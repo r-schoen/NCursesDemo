ccc89a33106a6d74cb83b9943a413ebb01cf9794
russell schoen <rschoen@fastmail.fm>
2020-03-08 12:41:22 -0500

    Initial Commit

---
+++ NCursesDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NCursesDemo
+//
+//  Created by Russell Schoen on 3/8/20.
+//  Copyright © 2020 Russell Schoen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
